950fab4b015bed1ea4582b3884f5d497ae869cac
cheikhfiteni <cheikhfiteni@gmail.com>
2024-03-18 18:01:36 -0400

    handling AWS Secret s Manager from the instance

---
+++ secrets.js
@@ -0,0 +1,63 @@
+// Use this code snippet in your app.
+// If you need more information about configurations or implementing the sample code, visit the AWS docs:
+// https://docs.aws.amazon.com/sdk-for-javascript/v3/developer-guide/getting-started.html
+
+const { SecretsManagerClient, GetSecretValueCommand } = require("@aws-sdk/client-secrets-manager");
+  
+const secret_name = "prod/Context-TA/env-variables/v1";
+
+const client = new SecretsManagerClient({
+region: "us-east-2",
+});
+
+//   let response;
+  
+//   try {
+//     response = await client.send(
+//       new GetSecretValueCommand({
+//         SecretId: secret_name,
+//         VersionStage: "AWSCURRENT", // VersionStage defaults to AWSCURRENT if unspecified
+//       })
+//     );
+//   } catch (error) {
+//     // For a list of exceptions thrown, see
+//     // https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+//     throw error;
+//   }
+  
+//   const secret = response.SecretString;
+  
+
+async function getSecret() {
+    try {
+      const response = await client.send(
+        new GetSecretValueCommand({
+          SecretId: secret_name,
+          VersionStage: "AWSCURRENT", // VersionStage defaults to AWSCURRENT if unspecified
+        })
+      );
+      if ('SecretString' in response) {
+        return JSON.parse(response.SecretString);
+      } else {
+        const buff = Buffer.from(response.SecretBinary, 'base64');
+        return JSON.parse(buff.toString('ascii'));
+      }
+    } catch (error) {
+      throw error;
+    }
+  }
+
+async function loadSecretsIntoEnv() {
+    try {
+        const secrets = await getSecret();
+        // Assuming the secrets are in key-value pairs
+        for (const [key, value] of Object.entries(secrets)) {
+            process.env[key] = value;
+        }
+    } catch (error) {
+        console.error('Error retrieving secrets', error);
+        // process.exit(0); // Exit the process non-fatally (ie keep commented)
+    }
+}
+
+module.exports = {loadSecretsIntoEnv};
