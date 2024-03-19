0ac54df2877683c55e6fdac5cd9fe42022f2a36f
cheikhfiteni <cheikhfiteni@gmail.com>
2024-03-19 01:35:01 -0400

    cors update

--- server.js
+++ server.js
@@ -41,6 +41,7 @@ loadSecretsIntoEnv().then(async () => {
         'https://context-asa06jm4w-envoy-intelligence.vercel.app',
         'https://context-fiq8a0eob-envoy-intelligence.vercel.app', // Removed the trailing slash here
         'https://context-ta.com',
+        'https://www.context-ta.com/',
         'https://context-ta.vercel.app',
         ],
         optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
