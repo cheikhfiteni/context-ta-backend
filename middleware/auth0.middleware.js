00b985d6732c4404aac74258f1904d503e99d404
cheikhfiteni <cheikhfiteni@gmail.com>
2024-03-19 12:54:32 -0400

    trying to figure out if server is misconfigured—if configured properly it should work properly?

--- middleware/auth0.middleware.js
+++ middleware/auth0.middleware.js
@@ -1,8 +1,16 @@
 const { auth } = require("express-oauth2-jwt-bearer");
 const { loadSecretsIntoEnv } = require('../secrets'); // Adjust the path as necessary
+const { writeErrorLog } = require('../logger');
 
 async function setupAuthMiddleware() {
   await loadSecretsIntoEnv(); // Ensure secrets are loaded before proceeding
+
+  // Print in red the environment variables to ensure they are loaded
+  // DELETE AFTER TESTING!!!
+
+  console.log('\x1b[31m%s\x1b[0m', process.env.AUTH0_DOMAIN);
+  console.log('\x1b[31m%s\x1b[0m', process.env.AUTH0_AUDIENCE);
+  writeErrorLog(`Error retrieving secrets: ${process.env.AUTH0_DOMAIN} \n \n  ${process.env.AUTH0_AUDIENCE}`, false);
 
   const validateAccessToken = auth({
     issuerBaseURL: `https://${process.env.AUTH0_DOMAIN}`,
